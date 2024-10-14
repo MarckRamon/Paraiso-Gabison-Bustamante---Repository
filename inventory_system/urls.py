785e4a8270073f5f06b03de7f2a91241f46ccc6a
MarckRamon <genhuntmason@gmail.com>
2024-10-14 13:04:52 +0800

    Redirect URL to Main Login Page
    Redirect URL to Main Login Page

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('inventory/', include('inventory.urls')),
     path('', include('inventory.urls')),
+    path('', lambda request: redirect('accounts/login/')),
 ]
